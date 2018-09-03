7e012d13cc98572e47815fdbbb1cd45dc7bc9a22
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-03 04:23:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,13 @@ function destructivelyRemoveFirstKitten(name){
 
 function appendKitten(name){
   return  [...kittens, name]
-
  }
+
+
+function prependKitten(name){
+  return [...name, kittens]
+}
+
+function removeLastKitten(name){
+  return [kittens.slice(0,kittens.length - 1)]
+}
