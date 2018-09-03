5bda16ac171528e95478dc87c2911021eca1b38f
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-03 04:27:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function destructivelyPrependKitten(name){
   return kittens
 }
 
-function destructivelyRemoveLastKitten(name){
-  kittens.pop(name)
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
   return kittens
 }
 
-function destructivelyRemoveFirstKitten(name){
-  kittens.shift(name)
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
   return kittens
 }
 
@@ -27,13 +27,13 @@ function appendKitten(name){
 
 
 function prependKitten(name){
-  return [...name, kittens]
+  return [name, ...kittens]
 }
 
-function removeLastKitten(name){
-  return [kittens.slice(0,kittens.length - 1)]
+function removeLastKitten(){
+  return [kittens.slice(0, kittens.length - 1)]
 }
 
-function removeFirstKitten(name){
+function removeFirstKitten(){
   return [kittens.slice(1)]
 }
